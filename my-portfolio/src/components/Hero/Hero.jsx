169e21de5879eff0f916c434d0a61c8268660114
ERANDI002 <ar112344@fhss.sjp.ac.lk>
2025-09-23 14:44:47 +0530

    initialize Tailwind CSS config

--- my-portfolio/src/components/Hero/Hero.jsx
+++ my-portfolio/src/components/Hero/Hero.jsx
@@ -13,15 +13,15 @@ const Hero =() => {
       {/*Left side: Social Media Icons*/}
         <div className = "absolute left-0 top-10 flex flex-col gap-4">
         
-         <a href ="www.linkedin.com/in/erandi-wickramaarachchi-6393b5312/" target="_blank" rel="noopener noreferrer" className = "p-3 bg white/10 rounded-full cursor-pointer hover:scale-110 ease-in duration-300">
+         <a href ="www.linkedin.com/in/erandi-wickramaarachchi-6393b5312/" target="_blank" rel="noopener noreferrer" className = "p-3 bg-white/10 rounded-full cursor-pointer hover:scale-110 ease-in duration-300">
          <FaLinkedin size={20} className="text-white"/>
          </a>
 
-         <a href ="https://github.com/ERANDI002" target="_blank" rel="noopener noreferrer" className = "p-3 bg white/10 rounded-full cursor-pointer hover:scale-110 ease-in duration-300">
+         <a href ="https://github.com/ERANDI002" target="_blank" rel="noopener noreferrer" className = "p-3 bg-white/10 rounded-full cursor-pointer hover:scale-110 ease-in duration-300">
          <FaGitHub size={20} className="text-white"/>
          </a>
 
-         <a href ="https://www.instagram.com/erandi_wickramaarachchi_/?utm_source=qr&igsh=bXl6ZGF0azVyaHlx#" target="_blank" rel="noopener noreferrer" className = "p-3 bg white/10 rounded-full cursor-pointer hover:scale-110 ease-in duration-300">
+         <a href ="https://www.instagram.com/erandi_wickramaarachchi_/?utm_source=qr&igsh=bXl6ZGF0azVyaHlx#" target="_blank" rel="noopener noreferrer" className = "p-3 bg-white/10 rounded-full cursor-pointer hover:scale-110 ease-in duration-300">
          <FaInstagram size={20} className="text-white"/>
          </a> 
 
@@ -30,7 +30,7 @@ const Hero =() => {
         {/* Center:Profile Picture */}
 
         <div className ="flex flex-col md:flex-row items-center justify-center gap-8 md:gap-16">
-        <div className = "relative w-64 h-64 rounded-full overflow - hidden border-4 border-purpule-600 shadow-lg">
+        <div className = "relative w-64 h-64 rounded-full overflow-hidden border-4 border-purple-600 shadow-lg">
            <img 
            src= "/erandi.png"
            alt="Profile"
