d71ae673eb079b84428fda83db88d25ec80ef738
ERANDI002 <ar112344@fhss.sjp.ac.lk>
2025-09-24 12:39:20 +0530

    add main portfolio layout with Hero, About, Skills, Projects, and Contact sections

---
+++ my-portfolio/src/components/MainPortfolio.jsx
@@ -0,0 +1,30 @@
+//src/components/MainPortfolio.jsx
+
+import React from 'react';
+import Hero from '../components/Hero/Hero.jsx';
+import About from'../pages/Projects.jsx';
+import Project from '../pages/Projects.jsx';
+import Contact from '../pages/Skills.jsx';
+import Skills from '../pages/Skills.jsx';
+
+const MainPorfolio = () => {
+    return (
+        <div>
+            <section id ="home">
+                <Hero/>
+            </section>
+            <section id ="about">
+                <About/>
+            </section>
+            <section id ="skills">
+                <Skills/>
+            </section>
+            <section id ="projects">
+                <Projects/>
+            </section>
+            <section id ="contact">
+                <Contact/>
+            </section>
+        </div>
+    )
+}
